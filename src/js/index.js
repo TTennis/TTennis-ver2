5bc5339e1bd40a3b7d5abac0cac750f5b54227fe
pavel.karachev <pavel.karachev@team.wrike.com>
2018-08-08 18:23:14 +0300

    Добавляет объект со стандартной игрой

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,24 @@
 let btnHomeNewGame = document.querySelector('.home__new-game');
+let btnHomeQuickGame = document.querySelector('.home__quick-game');
 
 let home = document.querySelector('.home');
 let settings = document.querySelector('.settings');
+let game = document.querySelector('.game');
+
+let standartGame = {
+  player1: 'Игрок 1',
+  player2: 'Игрок 2',
+  points: 21,
+  pitch: 5,
+};
 
 btnHomeNewGame.addEventListener('click', function () {
   nextStep(home, settings);
+});
+
+btnHomeQuickGame.addEventListener('click', function () {
+  nextStep(home, game);
+  quickGame();
 });
 
 function nextStep(prevBlock, nextBlock) {
@@ -14,4 +28,11 @@ function nextStep(prevBlock, nextBlock) {
 
   nextBlock.classList.add('visible');
   nextBlock.classList.remove('hidden');
+}
+
+function quickGame() {
+  let currentGame = {};
+  // currentGame.prototype = Object.create(standartGame.prototype);
+  currentGame.__proto__ = standartGame;
+  console.log(currentGame);
 }
